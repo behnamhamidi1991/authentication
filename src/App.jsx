7f7d3a47d4266d699c49ff1e25cd049c26acdb1c
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-23 12:46:03 +0330

    Fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,34 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const options = {
-  method: "GET",
-  url: "https://investing-cryptocurrency-markets.p.rapidapi.com/coins/get-news",
-  params: {
-    pair_ID: "1057391",
-    page: "1",
-    time_utc_offset: "28800",
-    lang_ID: "1",
-  },
-  headers: {
-    "X-RapidAPI-Key": "fa982306ecmsh438ba6bf7ad4885p191618jsn582b5ea5c40b",
-    "X-RapidAPI-Host": "investing-cryptocurrency-markets.p.rapidapi.com",
-  },
-};
-
 function App() {
-  useEffect(() => {
-    async function myApi() {
-      try {
-        const response = await axios.request(options);
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
+  async function getData() {
+    const url =
+      "https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr";
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "fa982306ecmsh438ba6bf7ad4885p191618jsn582b5ea5c40b",
+        "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+      },
+    };
 
-    myApi();
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
   }, []);
+
   return (
     <>
-      <div className="container">
-        <h1>Title</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-          dolores.
-        </p>
-      </div>
+      <div className="container"></div>
     </>
   );
 }
