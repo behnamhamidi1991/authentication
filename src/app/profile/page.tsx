d14af0b7217fa14fd6aea4f0bf4754677afba558
Behnam Hamidi <eslp91@hotmail.com>
2024-03-08 17:50:27 +0330

    logout done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,38 @@
+'use client';
 import React from 'react';
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
 
-const page = () => {
+const ProfilePage = () => {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      await axios.get('/api/users/logout');
+      toast.success('you logged out!');
+      router.push('/login');
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className="container px-10 py-5 text-2xl">
       <h1 className="font-black">Profile</h1>
       <p className="text-orange-400 font-medium">Welcome to your profile</p>
+      <hr />
+
+      <button
+        onClick={logout}
+        className="bg-blue-500 hover:bg-blue-700  text-white font-bold py-1 px-3 rounded-sm mt-4 text-[14px]"
+      >
+        Logout
+      </button>
     </div>
   );
 };
 
-export default page;
+export default ProfilePage;
