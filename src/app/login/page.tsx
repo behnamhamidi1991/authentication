859326d7cec2f28df18aeccd6cabe99682da4b9c
Behnam Hamidi <eslp91@hotmail.com>
2024-03-08 17:11:59 +0330

    Signup and Login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -38,7 +38,7 @@ const SigninPage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Login</h1>
+      <h1>{loading ? 'Loading ...' : 'Login'}</h1>
       <hr />
 
       <input
@@ -47,7 +47,7 @@ const SigninPage = () => {
         value={user.email}
         onChange={(e) => setUser({ ...user, email: e.target.value })}
         placeholder="email"
-        className="p-2 rounded-sm mt-1"
+        className="p-2 rounded-sm mt-1 text-black"
       />
       <input
         type="password"
@@ -55,7 +55,7 @@ const SigninPage = () => {
         value={user.password}
         onChange={(e) => setUser({ ...user, password: e.target.value })}
         placeholder="password"
-        className="p-2 rounded-sm mt-1"
+        className="p-2 rounded-sm mt-1  text-black"
       />
       <div className="btn-container flex gap-2 ">
         <button
