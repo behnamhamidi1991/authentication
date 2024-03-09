3c81ac6066cd1ac3c4789bdbcf7fab6aa0e49b36
Behnam Hamidi <eslp91@hotmail.com>
2024-03-09 10:03:28 +0330

    verification

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,1 +1,50 @@
+import nodemailer from 'nodemailer';
+import User from '@/models/userModel';
+import bcryptjs from 'bcryptjs';
 
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    // Create a hashed token
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    // Create a transporter
+    if (emailType === 'VERIFY') {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === 'RESET') {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: 'sandbox.smtp.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: '56ccc417d3a9bb',
+        pass: '9ef74141953fd7',
+        // TODO: add these credentials to .env file
+      },
+    });
+
+    const mailOptions = {
+      from: 'behnamhamidi688@gmail.com',
+      to: email,
+      subject:
+        emailType === 'VERIFY' ? 'Verify your email' : 'Reset your password',
+      html: `<p>Click <a href="${
+        process.env.domain
+      }/verifyemail?token=${hashedToken}" here </a> to ${
+        emailType === 'VERIFY' ? 'verify your email' : 'reset your password'
+      } </p>`,
+    };
+
+    const mailresponse = await transporter.sendMail(mailOptions);
+    return mailresponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
